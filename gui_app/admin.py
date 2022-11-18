aabc7eb49d04dcc4f32614ee505fec8d78322dc2
hui xie <26860579+huixie01@users.noreply.github.com>
2022-11-18 00:19:52 -0500

    Create admin.py

---
+++ gui_app/admin.py
@@ -0,0 +1,9 @@
+from gui_app.models import Node, Group, Application, Report, ReportCategory, ReportPlots
+from django.contrib import admin
+
+admin.site.register(Node)
+admin.site.register(Group)
+admin.site.register(Application)
+admin.site.register(Report)
+admin.site.register(ReportCategory)
+admin.site.register(ReportPlots)
